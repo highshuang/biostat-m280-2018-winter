008dd7c4f90697621bcfbadb6b9cf68ee1df0150
highshuang <gaoshuang@ucla.edu>
2018-01-31 23:17:36 +0000

    delete unnessessary files

---
+++ hw1/format.R
@@ -0,0 +1,61 @@
+
+library(knitr)
+
+# create an empty dataframe to store the output table 
+data <- data.frame(matrix(nrow = 10, ncol = 5))
+
+# create a list for all the text files in the working directory
+list.filename <- list.files(pattern = ".txt$")
+
+# create an empty list to store files
+list.data <- list()
+
+n <- length(list.filename)
+
+# create a loop to read in all the texts 
+for(i in 1:n){
+  list.data[[i]] <- read.table(list.filename[i])
+}
+
+# add the names of data to the list, so that can call the file directly
+names(list.filename) <- list.filename
+
+# write in the second column
+data[,2] <- c("PrimeAvg","SampAvg","PrimeAvg","SampAvg","PrimeAvg","SampAvg",
+             "PrimeAvg","SampAvg","PrimeAvg","SampAvg")
+
+# input data into data table
+for(i in 1:10){
+  # using index to specify the sample size
+  index <- as.integer((i+1)/2)
+  if(i %% 2 == 1){
+    data[i,1] <- index*100
+    
+  }
+}
+
+
+# select file with same sample size 
+fileindex <- seq(1,15,3)
+
+for(i in fileindex) {
+  for(j in 0:2){
+    temp <- list.data[[i+j]]
+    data[(i+2)/3*2-1,j+3] <- temp[1]
+    data[(i+2)/3*2,j+3] <- temp[2]
+  }
+}
+
+output <- cbind(data[,-3],data[,3])
+
+
+colname <- c("n","Method","t1","t5","Gaussian")
+colnames(output) <- colname
+
+kable(output,"markdown")
+
+
+
+
+
+
---
+++ hw1/runSim.R
@@ -0,0 +1,60 @@
+## parsing command arguments
+for (arg in commandArgs(TRUE)) {
+  eval(parse(text=arg))
+}
+
+## check if a given integer is prime
+isPrime = function(n) {
+  if (n <= 3) {
+    return (TRUE)
+  }
+  if (any((n %% 2:floor(sqrt(n))) == 0)) {
+    return (FALSE)
+  }
+  return (TRUE)
+}
+
+# set the seed outside the function
+set.seed(seed)
+## estimate mean only using observation with prime indices
+estMeanPrimes = function (n,dist,rep) {
+  # set seed to generate the same result each time
+  
+  ind = sapply(1:n, isPrime)
+  sumPrime <- 0
+  sumSamp <- 0
+  
+  if(dist=="gaussian"){
+    for(i in 1:rep){
+      x <- rnorm(n)
+      sumPrime <- sumPrime+ mean(x[ind])^2
+      sumSamp <- sumSamp + mean(x)^2
+    }
+    return( data.frame(sumPrime/rep,sumSamp/rep))
+  } else if(dist == "t1"){
+    
+    for(i in 1:rep){
+      x <- rt(n,1)
+      sumPrime <- sumPrime+ mean(x[ind])^2
+      sumSamp <- sumSamp + mean(x)^2
+    }
+    return( data.frame(sumPrime/rep,sumSamp/rep))
+  } else {
+    for(i in 1:rep){
+      x <- rt(n,5)
+      sumPrime <- sumPrime+ mean(x[ind])^2
+      sumSamp <- sumSamp + mean(x)^2
+    }
+    return(data.frame(sumPrime/rep,sumSamp/rep))
+  }
+
+}
+
+# estimate mean with given 4 paramters
+estMeanPrimes(n,dist,rep)
+
+
+
+
+
+
