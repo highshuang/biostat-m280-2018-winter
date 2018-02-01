4ee7f65b97d853a281db788ceb3df331850db844
highshuang <gaoshuang@ucla.edu>
2018-02-01 01:35:39 +0000

    change the format of runSim.R

--- hw1/runSim.R
+++ hw1/runSim.R
@@ -4,54 +4,58 @@ for (arg in commandArgs(TRUE)) {
 }
 
 ## check if a given integer is prime
-isPrime = function(n) {
+isPrime <- function(n) {
   if (n <= 3) {
     return (TRUE)
   }
   if (any((n %% 2:floor(sqrt(n))) == 0)) {
-    return (FALSE)
+    return(FALSE)
   }
-  return (TRUE)
+  return(TRUE)
 }
 
 # set the seed outside the function
 set.seed(seed)
-## estimate mean only using observation with prime indices
-estMeanPrimes = function (n,dist,rep) {
+
+# estimate mean only using observation with prime indices
+estMeanPrimes <- function (n, dist, rep) {
   # set seed to generate the same result each time
   
-  ind = sapply(1:n, isPrime)
+  ind <- sapply(1:n, isPrime)
   sumPrime <- 0
   sumSamp <- 0
   
-  if(dist=="gaussian"){
-    for(i in 1:rep){
+  # generate data according to the dist input
+  # 
+  if (dist == "gaussian"){
+    for (i in 1:rep){
       x <- rnorm(n)
-      sumPrime <- sumPrime+ mean(x[ind])^2
+      sumPrime <- sumPrime + mean(x[ind])^2
       sumSamp <- sumSamp + mean(x)^2
     }
-    return( data.frame(sumPrime/rep,sumSamp/rep))
-  } else if(dist == "t1"){
+    return(data.frame(sumPrime/rep, sumSamp/rep))
+  } else if (dist == "t1"){
     
-    for(i in 1:rep){
+    for (i in 1:rep){
       x <- rt(n,1)
-      sumPrime <- sumPrime+ mean(x[ind])^2
+      sumPrime <- sumPrime + mean(x[ind])^2
       sumSamp <- sumSamp + mean(x)^2
     }
-    return( data.frame(sumPrime/rep,sumSamp/rep))
+    return(data.frame(sumPrime/rep, sumSamp/rep))
   } else {
-    for(i in 1:rep){
+    for (i in 1:rep){
       x <- rt(n,5)
-      sumPrime <- sumPrime+ mean(x[ind])^2
+      sumPrime <- sumPrime + mean(x[ind])^2
       sumSamp <- sumSamp + mean(x)^2
     }
-    return(data.frame(sumPrime/rep,sumSamp/rep))
+    return(data.frame(sumPrime/rep, sumSamp/rep))
   }
 
 }
 
 # estimate mean with given 4 paramters
 estMeanPrimes(n,dist,rep)
+
 
 
 
