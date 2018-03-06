92a94a0ab47d6260b61271f2336495e9808c9b3b
highshuang <highshuang@gmail.com>
2018-03-06 22:02:10 +0000

    test ssh key

---
+++ hw4/hw4.R
@@ -0,0 +1,34 @@
+# connect to spark
+library(sparklyr)
+library(dplyr)
+library(ggplot2)
+
+# specify where spark is
+Sys.setenv(SPARK_HOME="/usr/lib/spark")
+config <- spark_config()
+sc <- spark_connect(master = "yarn-client", config = config)
+
+# show tables 
+sc   
+
+
+# Cache flights Hive table into Spark
+#tbl_cache(sc, 'flights')
+flights_tbl <- tbl(sc, 'flights') #create pointer to the table
+flights_tbl %>% print(width = Inf)
+
+# Cache airports Hive table into Spark
+#tbl_cache(sc, 'airports')
+airports_tbl <- tbl(sc, 'airports')
+airports_tbl %>% print(width = Inf)
+
+
+# total data point by year
+out <- flights_tbl %>%
+  group_by(year) %>%
+  count() %>%
+  arrange(year) %>%
+  collect()
+
+
+
